f64410a3bcf64259908aadc782789e342a57870f
ZLI3000 <zehnlising@proton.me>
2024-04-06 18:20:55 -0700

    added coqui TTS and successfully scrapes hot post on reddit and creates ai voice over

--- tts.py
+++ tts.py
@@ -1,7 +1,19 @@
-from tortoise import api
-from tortoise import utils
-clips_paths = '/home/egg/tortoise-tts/tortoise/voices/deniro/1.wav'
+import torch
+from main import ns
+from TTS.api import TTS
 
-reference_clips = [utils.audio.load_audio('/home/egg/tortoise-tts/tortoise/voices/deniro/1.wav', 22050) for p in clips_paths]
-tts = api.TextToSpeech(use_deepspeed=True, kv_cache=True, half=True)
-pcm_audio = tts.tts_with_preset("your text here", voice_samples=reference_clips, preset='fast')
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+with open("nosleep.txt", "r") as ns:  # Run TTS
+    text = ns.read()
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech to a file
+tts.tts_to_file(text=text, speaker_wav="/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/TTS/voices/emma/1.wav", language="en", file_path="output.wav")
+

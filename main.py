118325f08288d53db87573cb0aae9168ebd0d866
ZLI3000 <zehnlising@proton.me>
2024-04-07 18:31:10 -0700

    Seperated Tortoise TTS scripts and TTSxv2 Coqui scripts, tortoise is too slow yet quality is better managed

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import torch
+import torchaudio
+# from TTS.tts.configs.tortoise_config import TortoiseConfig
+# from TTS.tts.models.tortoise import Tortoise
+from TTS.api import TTS
+from pathlib import Path
 import praw
 
 reddit = praw.Reddit(
@@ -6,11 +12,32 @@ reddit = praw.Reddit(
     user_agent='linux:com.nosleepscraper:0.1 (by /u/Middle_Reputation310)',
 )
 
-subreddit = reddit.subreddit("nosleep") #grabs subreddit ID and attaches var
-hot_submissions = subreddit.hot(limit=5) #creates variable for hot submissions
-for submission in hot_submissions: #grabbing hot submissions
-    with open("nosleep.txt", "w") as ns: #opens nosleep.txt in write mode ns is the nosleep file path
-        print(submission.selftext, file=ns) #self text grabs the submissions text and the print is forwarded to a txt file
+subreddit = reddit.subreddit("nosleep")  # grabs subreddit ID and attaches var
+hot_submissions = subreddit.hot(limit=5)  # creates variable for hot submissions
+for submission in hot_submissions:  # grabbing hot submissions
+    with open("nosleep.txt", "w") as ns:  # opens nosleep.txt in write mode ns is the nosleep file path
+        print(submission.selftext,
+              file=ns)  # self text grabs the submissions text and the print is forwarded to a txt file
 
+device = "cuda" if torch.cuda.is_available() else "cpu"  # specifies to use CUDA cores
+# config = TortoiseConfig()  # uses tortoise config
+# model = Tortoise.init_from_config(config)  # initializes model from tortoise config
+# modelPath = Path('/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/tortoise/models/')
+# voicePath = Path(
+#     "/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/tortoise/voices/")
+speakerPath = Path(
+    "/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/tortoise/voices/geralt/3.wav")
+# model.load_checkpoint(config,
+#                       checkpoint_dir=modelPath,
+#                       eval=True)
+# finds path to use models
+# tts = TTS("tts_models/en/multi-dataset/tortoise-v2")  # sets variable tts to use tortoise models specified in
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
+with open("nosleep.txt", "r") as ns:
+    text = ns.read()
 
+tts.tts_to_file(text=text,
+                file_path="output.wav",
+                speaker_wav=speakerPath,
+                language='en')
--- tts.py
+++ tts.py
@@ -1,19 +1,41 @@
 import torch
-from main import ns
+import torchaudio
+from TTS.tts.configs.tortoise_config import TortoiseConfig
+from TTS.tts.models.tortoise import Tortoise
 from TTS.api import TTS
+from pathlib import Path
+import praw
 
-# Get device
-device = "cuda" if torch.cuda.is_available() else "cpu"
+reddit = praw.Reddit(
+    client_id='fXy-16VprltamlV0kpEuDQ',
+    client_secret='_tXlzrPE-34k5AvsxWXX42wasLVS0g',
+    user_agent='linux:com.nosleepscraper:0.1 (by /u/Middle_Reputation310)',
+)
 
-# List available 🐸TTS models
-print(TTS().list_models())
+subreddit = reddit.subreddit("nosleep")  # grabs subreddit ID and attaches var
+hot_submissions = subreddit.hot(limit=5)  # creates variable for hot submissions
+for submission in hot_submissions:  # grabbing hot submissions
+    with open("nosleep.txt", "w") as ns:  # opens nosleep.txt in write mode ns is the nosleep file
+        print(submission.selftext,
+              file=ns)  # self text grabs the submissions text and the print is forwarded to a txt file
 
-# Init TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+with open("nosleep.txt", "r") as ns:  # grabs the text file we just created and reads it
+    text = ns.read()  # passes a read function to the text file and passes a var
 
-with open("nosleep.txt", "r") as ns:  # Run TTS
-    text = ns.read()
-# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-# Text to speech to a file
-tts.tts_to_file(text=text, speaker_wav="/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/TTS/voices/emma/1.wav", language="en", file_path="output.wav")
+config = TortoiseConfig()  # uses tortoise config
+model = Tortoise.init_from_config(config)  # initializes model from tortoise config
+modelPath = Path('/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/tortoise/models/')
+voicePath = Path(
+    "/home/egg/PycharmProjects/RedditScraperRevised/.venv/lib/python3.11/site-packages/tortoise/voices/")
+#  sets path
+model.load_checkpoint(config,
+                      checkpoint_dir=modelPath,
+                      eval=True)
+tts = TTS("tts_models/en/multi-dataset/tortoise-v2")  # sets var tts to use tortoise models
 
+tts.tts_to_file(text=text,
+                file_path="tortoise.wav",
+                voice_dir=voicePath,
+                speaker="geralt",
+                num_autoregressive_samples=1,
+                diffusion_iterations=10)
